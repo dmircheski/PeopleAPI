e7477fe04fe8f2c09bad113292552aab06f918c3
dmircheski <deni.mirceski@t.mk>
2021-06-18 17:50:00 +0200

    implemented authorization on this branch

--- app/routes/person.route.js
+++ app/routes/person.route.js
@@ -1,14 +1,16 @@
+const middleware = require('../middleware/middleware')
+
 module.exports = (app) => {
-    const people = require('/Users/deniv/Desktop/API/app/controllers/person.controller')
-    // require('.controllers/person.controller')
+    const people = require('../controllers/person.controller')
+
 
     const router = require("express").Router();
 
-    router.post('/person', people.create);
-    router.get('/people', people.findAll);
-    router.get('/person/:personId', people.findOne);
-    router.put('/person/:personId', people.update);
-    router.delete('/person/:personId', people.delete);
+    router.post('/person', middleware.authenticateToken, people.create);
+    router.get('/people', middleware.authenticateToken, people.findAll);
+    router.get('/person/:personId', middleware.authenticateToken, people.findOne);
+    router.put('/person/:personId', middleware.authenticateTokenAndAdminRole, people.update);
+    router.delete('/person/:personId', middleware.authenticateTokenAndAdminRole, people.delete);
 
     app.use('/api', router);
 }
